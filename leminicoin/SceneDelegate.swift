fc72133a78c140bfa1ec7997ee7998070f3c34b6
psemler <pierre.semler@traveldoo.com>
2021-05-14 15:53:04 +0200

    Init xCode project with min iOS version to 12. Make it compatible with iOS 13 and the new SceneDelegate system

---
+++ leminicoin/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  leminicoin
+//
+//  Created by Pierre Semler on 14/05/2021.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
+
