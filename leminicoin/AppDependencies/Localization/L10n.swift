b4b94a03d6dc22f1227e3df7febe62af0e179105
psemler <pierre.semler@traveldoo.com>
2021-05-17 14:23:51 +0200

    Add localization to the app for English and French. If device language is french, app will be translated in french, if device language is english or another language, english translation will be displayed.

---
+++ leminicoin/AppDependencies/Localization/L10n.swift
@@ -0,0 +1,56 @@
+//
+//  L10n.swift
+//  leminicoin
+//
+//  Created by Pierre Semler on 17/05/2021.
+//
+
+import Foundation
+
+public enum L10n {
+    
+    public enum AdList {
+        
+        public enum NavigationItem {
+            
+            public static let title = L10n.getLocalizedString(table: "Localizable", key: "adList.navigationItem.title")
+        }
+        
+        public enum Filter {
+            
+            public enum Reset {
+                
+                public enum Button {
+                    
+                    public static let title = L10n.getLocalizedString(table: "Localizable", key: "adList.filter.reset.button.title")
+                }
+            }
+            
+            public static let title = L10n.getLocalizedString(table: "Localizable", key: "adList.filter.title")
+        }
+        
+        public enum SearchBar {
+            public static let placeholder = L10n.getLocalizedString(table: "Localizable", key: "adList.serchBar.placeholder")
+        }
+    }
+    
+    public enum Error {
+        public enum Generic {
+            public static let title = L10n.getLocalizedString(table: "Localizable", key: "error.generic.title")
+            public static let message = L10n.getLocalizedString(table: "Localizable", key: "error.generic.message")
+        }
+    }
+    
+    public enum PullToRefresh {
+        public static let message = L10n.getLocalizedString(table: "Localizable", key: "pullToRefresh.message")
+    }
+
+}
+
+extension L10n {
+    private static func getLocalizedString(table: String, key: String, args: CVarArg...) -> String {
+        let format = NSLocalizedString(key, tableName: table, bundle: Bundle.main, comment: "")
+        let str = args.count > 0 ? String(format: format, locale: .current, args) : format
+        return (key != str) ? str : "N/A"
+    }
+}
